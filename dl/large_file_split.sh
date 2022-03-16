1da2bcb2e01e411cc430f9a1288204ddd9c2ff43
笨蛋海绵 <1037886804@qq.com>
2022-03-16 03:35:26 +0000

    Auto Update

--- dl/large_file_split.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# 将large_file.list内的大文件分割为小于45MB的多个文件，会生成FILENAME.aa FILENAME.ab等后缀的文件
-
-for filename in `cat large_file.list`
-do
-	echo split ${filename}
-	split -b 45M ${filename} ${filename}.
-done
